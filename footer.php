fadc071d5225cf74c80feb9ee13de5f2803088cd
galoyanedgar <galoyanedgar@gmail.com>
2018-04-15 19:27:57 -0400

    Last commit

--- footer.php
+++ footer.php
@@ -1,7 +1,11 @@
+<?php $theme_opts = get_option( 'tu_opts' ); ?>    
+
     <footer class="footer">
         <hr class="footer-hr">
         <div class="container">
-            <div class="text-muted mt-4">© 2017 - 2018 by PHP-User. All rights reserved.</div>
+            <?php if ( isset($theme_opts['footer']) && ! empty($theme_opts['footer']) ) : ?>
+            <div class="text-muted mt-4"><?php echo $theme_opts['footer']; ?></div>
+            <?php endif; ?>
             <div class="text-muted mt-2"><a id="back-to-top" href="#">Back to top</a></div>
         </div>
     </footer> 
--- header.php
+++ header.php
@@ -1,16 +1,30 @@
+<?php $theme_opts = get_option( 'tu_opts' ); ?>
+
 <!doctype html>
 <html <?php language_attributes(); ?>>
   <head>
     <!-- Required meta tags -->
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <?php if ( isset($theme_opts['favicon']) ) : ?>      
+        <link rel="icon" href="<?php echo $theme_opts['favicon']; ?>" type="image/x-icon" />
+        <link rel="shortcut icon" href="<?php echo $theme_opts['favicon']; ?>" type="image/x-icon" />
+    <?php endif; ?>
+      
     <?php wp_head(); ?>
   </head>
   <body>
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
           <div class="container">
-          
-              <a class="navbar-brand" href="<?php echo site_url(); ?>">My Blog</a>
+              
+              <?php if ( isset($theme_opts['logo-type']) && $theme_opts['logo-type'] == 2 ) : ?>
+                  <a class="navbar-brand" href="<?php echo site_url(); ?>">
+                      <img src="<?php echo $theme_opts['logo-image']; ?>" class="img-fluid" alt="logo-img">
+                  </a>
+              <?php else : ?>
+                  <a class="navbar-brand" href="<?php echo site_url(); ?>"><?php bloginfo( 'name' ); ?></a>
+              <?php endif; ?>
+
               <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
